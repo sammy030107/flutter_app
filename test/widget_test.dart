a0d96f6ee25fd07603f2ec95be7c65779a595c01
student <sammy030107@gmail.com>
2022-07-22 15:37:01 +0800

    int a; int b;
    a = 0;
    b = ++a; // Increment a before b gets its value.
    assert(a == b); // 1 == 1
    
    a = 0;
    b = a++; // Increment a AFTER b gets its value.
    assert(a != b); // 1 != 0
    print(a);
    print(b);
    
    a = 0;
    b = --a; // Decrement a before b gets its value.
    assert(a == b); // -1 == -1
    print(a);
    print(b);
    
    a = 0;
    b = a--; // Decrement a AFTER b gets its value.
    assert(a != b); // -1 != 0
    print(a);
    print(b);

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -28,3 +28,27 @@ void main() {
     expect(find.text('1'), findsOneWidget);
   });
 }
+int a;
+int b;
+
+a = 0;
+b = ++a; // Increment a before b gets its value.
+assert(a == b); // 1 == 1
+
+a = 0;
+b = a++; // Increment a AFTER b gets its value.
+assert(a != b); // 1 != 0
+print(a);
+print(b);
+
+a = 0;
+b = --a; // Decrement a before b gets its value.
+assert(a == b); // -1 == -1
+print(a);
+print(b);
+
+a = 0;
+b = a--; // Decrement a AFTER b gets its value.
+assert(a != b); // -1 != 0
+print(a);
+print(b);
